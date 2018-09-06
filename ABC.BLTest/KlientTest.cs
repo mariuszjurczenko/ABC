ebbd7a428349dc15c78f15c082b76bb166c07339
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-09-06 13:27:45 +0200

    3_5 Testowanie klasy Klient

---
+++ ABC.BLTest/KlientTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class KlientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            // Arrange (zaranżuj test)
+            Klient klient = new Klient();
+            klient.Imie = "Tomasz";
+            klient.Nazwisko = "Nowak";
+            string oczekiwana = "Tomasz, Nowak";
+
+            //ACT (działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            // Assert (potwierdz test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+    }
+}
---
+++ ABC.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ABC.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ABC.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c7a42c7-c34f-42eb-9fd4-1f551ea0ec9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
