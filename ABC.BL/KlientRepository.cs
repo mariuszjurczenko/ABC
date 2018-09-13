665cb4768a49c11b8db164c6944939d35d8ca9cf
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-09-13 10:27:09 +0200

    5_4 dodajemy KlientRepository

---
+++ ABC.BL/KlientRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace ABC.BL
+{
+    public class KlientRepository
+    {
+        /// <summary>
+        /// Pobieramy jednego klienta.
+        /// </summary>
+        public Klient Pobierz(int klientId)
+        {
+            // Tworzymy instancję klasy Klienta
+            Klient klient = new Klient(klientId);
+
+            // kod, który pobiera określonego klienta
+
+            // Tymczasowo zakodowane wartości, aby zwrocic klienta
+            if (klientId == 1)
+            {
+                klient.Email = "marcin#dev-hobby.pl";
+                klient.Imie = "Marcin";
+                klient.Nazwisko = "Nowak";
+            }
+
+            return new Klient();
+        }
+
+        /// <summary>
+        /// Pobieramy wszystkich klientów.
+        /// </summary>
+        public List<Klient> Pobierz()
+        {
+            // Kod, który pobiera wszystkich klientów
+            return new List<Klient>();
+        }
+
+        /// <summary>
+        /// Zapisujemy obecnego klienta
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // Kod, który zapisuje zdefiniowanego klienta
+            return true;
+        }
+    }
+}
