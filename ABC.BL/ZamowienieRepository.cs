1c507b5be49f6032234bcfd307f84f77de3f895c
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-09-14 11:28:25 +0200

    6_10 dodanie WyswietlanieZamowienia, WyswietlaniePozycjiZamowienia, metoda PobierzZamowienieDoWyswietlenia

--- ABC.BL/ZamowienieRepository.cs
+++ ABC.BL/ZamowienieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ABC.BL
 {
@@ -21,6 +22,60 @@ namespace ABC.BL
                 zamowienie.DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
 
             return zamowienie;
+        }
+
+        /// <summary>
+        /// Pobieramy jedno zamówienie do wyswietlenia
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public WyswietlanieZamowienia PobierzZamowienieDoWyswietlenia(int zamowienieId)
+        {
+            WyswietlanieZamowienia wyswietlanieZamowienia = new WyswietlanieZamowienia();
+
+            // Kod, który pobiera zdefiniowane pola zamowienia
+
+            // Tymczasowe dane zakodowane na stałe
+            if (zamowienieId == 10)
+            {
+                wyswietlanieZamowienia.Imie = "Marcin";
+                wyswietlanieZamowienia.Nazwisko = "Nwak";
+                wyswietlanieZamowienia.DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+                wyswietlanieZamowienia.AdresDostawy = new Adres()
+                {
+                    AdresTyp = 1,
+                    Ulica = "Kosmiczna",
+                    Miasto = "Katowice",
+                    KodPocztowy = "40-467",
+                    Kraj = "Polska"
+                };
+            }
+
+            wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista = new List<WyswietlaniePozycjiZamowienia>();
+
+            // Kod, który pobiera elementy zamówienia
+
+            // Tymczasowe dane zakodowane na stałe
+            if (zamowienieId == 10)
+            {
+                var wyswietlaniePozycjiZamowienia = new WyswietlaniePozycjiZamowienia()
+                {
+                    NazwaProduktu = "Krzesło",
+                    IloscZamowienia = 4,
+                    CenaZakupu = 119.77M
+                };
+                wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista.Add(wyswietlaniePozycjiZamowienia);
+
+                wyswietlaniePozycjiZamowienia = new WyswietlaniePozycjiZamowienia()
+                {
+                    NazwaProduktu = "Stolik",
+                    IloscZamowienia = 7,
+                    CenaZakupu = 249M
+                };
+                wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista.Add(wyswietlaniePozycjiZamowienia);
+            }
+
+            return wyswietlanieZamowienia;
         }
 
         /// <summary>
