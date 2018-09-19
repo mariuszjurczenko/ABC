f27441e78df22608f41e43e95d922390b7f61f50
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-09-19 09:55:52 +0200

    8_3 Nowe Testy

---
+++ CommonTest/ObslugaStringaTest.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonTest
+{
+    [TestClass]
+    public class ObslugaStringaTest
+    {
+        [TestMethod]
+        public void WstawSpacjeTest() 
+        {
+            // Arange
+            var zrodlo = "KlockiLego";
+            var oczekiwana = "Klocki Lego";
+            var obslugaStringa = new ObslugaStringa();
+
+            // Act
+            var aktualna = obslugaStringa.WstawSpacje(zrodlo);
+
+            // Assert
+            Assert.AreEqual(oczekiwana, aktualna);
+
+        }
+
+        [TestMethod]
+        public void WstawSpacjeTestZeSpacja() 
+        {
+            // Arange
+            var zrodlo = "Klocki Lego";
+            var oczekiwana = "Klocki Lego";
+            var obslugaStringa = new ObslugaStringa();
+
+            // Act
+            var aktualna = obslugaStringa.WstawSpacje(zrodlo);
+
+            // Assert
+            Assert.AreEqual(oczekiwana, aktualna);
+
+        }
+    }
+}
---
+++ CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3cc74d7e-a17b-4988-81d8-8c76b8c395ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
